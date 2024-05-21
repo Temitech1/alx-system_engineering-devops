cd81344c18c041ccdc3cd7b309a2a022a1f88413
Opeyemi1122 <alabiopex@gmail.com>
2024-05-21 16:59:58 +0200

    dictionary of dictionaries of lists

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,58 +1,35 @@
 #!/usr/bin/python3
-"""
-Using a REST API, exports all tasks from all employees to a JSON file.
-"""
+"""python script to fetch Rest API for todo lists of employees"""
+
 import json
 import requests
+import sys
 
 
-def get_user_data(users, user_id):
-    """
-    Retrieve the user's name and username from the users list.
-    """
-    for user in users:
-        if user["id"] == user_id:
-            return user["name"], user["username"]
-    return "Unknown", "Unknown"
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/users"
 
+    resp = requests.get(url)
+    Users = resp.json()
 
-if __name__ == "__main__":
-    users_url = "https://jsonplaceholder.typicode.com/users"
-    todos_url = "https://jsonplaceholder.typicode.com/todos"
+    users_dict = {}
+    for user in Users:
+        USER_ID = user.get('id')
+        USERNAME = user.get('username')
+        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(USER_ID)
+        url = url + '/todos/'
+        resp = requests.get(url)
 
-    users_response = requests.get(users_url)
-    if users_response.status_code != 200:
-        print("Failed to fetch users.")
-        exit(1)
-
-    users = users_response.json()
-
-    todos_response = requests.get(todos_url)
-    if todos_response.status_code != 200:
-        print("Failed to fetch TODO list.")
-        exit(1)
-
-    todos = todos_response.json()
-
-    # Organize tasks by user ID
-    tasks_by_user = {}
-    for todo in todos:
-        user_id = todo["userId"]
-        user_name, user_username = get_user_data(users, user_id)
-        task_data = {
-            "username": user_username,
-            "task": todo["title"],
-            "completed": todo["completed"]
-        }
-
-        if user_id not in tasks_by_user:
-            tasks_by_user[user_id] = []
-
-        tasks_by_user[user_id].append(task_data)
-
-    # Export all tasks to JSON
-    json_filename = "todo_all_employees.json"
-    with open(json_filename, "w") as jsonfile:
-        json.dump(tasks_by_user, jsonfile, indent=4)
-
-    print(f"Task data exported to {json_filename}")
+        tasks = resp.json()
+        users_dict[USER_ID] = []
+        for task in tasks:
+            TASK_COMPLETED_STATUS = task.get('completed')
+            TASK_TITLE = task.get('title')
+            users_dict[USER_ID].append({
+                "task": TASK_TITLE,
+                "completed": TASK_COMPLETED_STATUS,
+                "username": USERNAME
+            })
+            """A little Something"""
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(users_dict, f)
